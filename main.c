4cf53e1b23c8c6a9131a8169c228d70c3220b167
Theeranai Nakpradit <9nongarm@gmail.com>
2020-08-16 04:56:37 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+  int a,b;
+  printf("Enter A : ");
+  scanf("%d",&a);
+  printf("Enter B : ");
+  scanf("%d",&b);
+  printf("%d + %d = %d",a,b,a+b);
+  return 0;
+}
